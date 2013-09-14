39b0a7800ed538d1aec078fbeac319bfccf62c79
Benjamin Dunphy <dunphy.ben@gmail.com>
2013-09-14 16:12:35 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firehoseapp::Application.config.secret_token = '1c5e1e7c327b36d0497a760479e2401d42f371a8c457b4d79850e4cde9b0edae5f756696500225856baee5c63cfa9c5c1123af8b09534ebaf9c20510f1a52edf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Firehoseapp::Application.config.session_store :cookie_store, key: '_firehoseapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Firehoseapp::Application.config.session_store :active_record_store
